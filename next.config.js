2cb2e2f07eba5d8e6741e466ec61da92be2d1648
Marina Diubailo <marinadiubailo@gmail.com>
2024-08-30 16:18:34 +0300

    refactor: refactor next.config

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@
 /** @type {import('next').NextConfig} */
 const withSvgr = require('next-plugin-svgr')
 
-const nextConfig = withSvgr({
+const nextConfig = {
+  basePath: '/next-courses',
+  output: 'export',
   reactStrictMode: true,
   webpack(config) {
     return config
   },
-})
+}
 
-module.exports = nextConfig
+module.exports = withSvgr(nextConfig)
